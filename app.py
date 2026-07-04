c9bdc56be96b1711e7518a66b57d9cc214973c1b
jhonanthonyf-arch <jhonanthonyf@gmail.com>
2026-07-04 18:09:03 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("Graficas de series de tiempo")
+
+entrada=st.text_input("Ingrese los datos separados por coma:",value="18,15,18,26,31")
+entrada2=entrada.split(",")
+
+serie=[float(i) for i in entrada2]
+st.line_chart(serie)
+
+
+
